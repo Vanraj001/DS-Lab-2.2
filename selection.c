4254a0255fabfbef53783df92add1a4eef11a4d7
Vanraj001 <165643811+Vanraj001@users.noreply.github.com>
2024-04-05 22:45:17 +0530

    Update selection.c

--- selection.c
+++ selection.c
@@ -2,27 +2,28 @@
 
 void display(int list[], int n)
 {
-int i;
-for(i=0; i<n; i++)
-printf("%d ", list[i]);
-}void selectionsort(int list[], int n)
-{
-int indexMin,i,j,temp;
-
-for(i=0; i<n-1; i++)
-{
-
-indexMin = i;
-
-for(j=i+1; j<n; j++)
-{
-if(list[j] < list[indexMin])
-indexMin = j;
+ int i;
+ for(i=0; i<n; i++)
+ printf("%d ", list[i]);
 }
-if(indexMin != i) {
-printf("\nItems swapped: [ %d, %d ]\n" , list[i], list[indexMin]);
+void selectionsort(int list[], int n)
+{
+  int indexMin,i,j,temp;
 
-temp = list[indexMin];
+  for(i=0; i<n-1; i++)
+  {
+
+  indexMin = i;
+
+ for(j=i+1; j<n; j++)
+{
+  if(list[j] < list[indexMin])
+  indexMin = j;
+}
+ if(indexMin != i) {
+ printf("\nItems swapped: [ %d, %d ]\n" , list[i], list[indexMin]);
+
+ temp = list[indexMin];
 list[indexMin] = list[i];
 list[i] = temp;
 }
@@ -32,17 +33,18 @@ display(list, n);
 }
 void main()
 {
-int list[100],i,n;
-clrscr();
-printf("\nHowmany elements do you want? : ");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{ printf("Enter the No [%d]: ",i+1);
-scanf("%d",&list[i]);
+  int list[100],i,n;
+  clrscr();
+  printf("\nHowmany elements do you want? : ");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+{ 
+  printf("Enter the No [%d]: ",i+1);
+  scanf("%d",&list[i]);
 }
-selectionsort(list,n);
-printf("\n\nAfter Sorting:\n");
-for(i=0;i<n;i++)
-printf("\t %d",list[i]);
-getch();
+  selectionsort(list,n);
+  printf("\n\nAfter Sorting:\n");
+  for(i=0;i<n;i++)
+  printf("\t %d",list[i]);
+  getch();
 }
